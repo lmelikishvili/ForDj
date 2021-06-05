72096f91b73088ef176b3061045e3a4009af4fda
lmelikishvili <levanimelikishvili@gmail.com>
2021-06-05 18:50:57 +0400

    Added Questions Model

--- personal/views.py
+++ personal/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from personal.models import Questions
 
 # Create your views here.
 
 def home_screen_view(request):
     context = {}
-    list_of_values = [
-        "Levani",
-        "Melikishvili",
-        33
-    ]
-    context['list_content'] = list_of_values
-
+    # list_of_values = [
+    #     "Levani",
+    #     "Melikishvili",
+    #     33
+    # ]
+    # context['list_content'] = list_of_values
+    quests = Questions.objects.all()
+    context['questions'] = quests
     return render(request, 'personal/home.html', context)
