fb635641bc40b6b0d8461451365f8eac98358e22
lmelikishvili <levanimelikishvili@gmail.com>
2021-06-05 18:10:59 +0400

    adding superuser

--- personal/views.py
+++ personal/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-    return render(request, 'base.html', {})
+    context = {}
+    list_of_values = [
+        "Levani",
+        "Melikishvili",
+        33
+    ]
+    context['list_content'] = list_of_values
+
+    return render(request, 'personal/home.html', context)
