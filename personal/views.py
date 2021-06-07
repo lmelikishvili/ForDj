530bc823a9d9490d43ceceb4e60773712c251f28
lmelikishvili <levanimelikishvili@gmail.com>
2021-06-07 20:51:58 +0400

    fixedFiles

--- personal/views.py
+++ personal/views.py
@@ -5,12 +5,12 @@ from personal.models import Questions
 
 def home_screen_view(request):
     context = {}
-    # list_of_values = [
-    #     "Levani",
-    #     "Melikishvili",
-    #     33
-    # ]
-    # context['list_content'] = list_of_values
+    list_of_values = [
+        "Levani",
+        "Melikishvili",
+        33
+    ]
+    context['list_content'] = list_of_values
     quests = Questions.objects.all()
     context['questions'] = quests
     return render(request, 'personal/home.html', context)
